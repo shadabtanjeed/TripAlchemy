bba952b0dd4c7d706afaa3ed617d1a02b97f3f97
shadabtanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2024-12-09 09:25:58 +0600

    created signup and signin page

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.db.utils import DatabaseError
+import hashlib
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
 
-# Create your views here.
+
+def index(request):
+    return render(request, "login_page.html")
+
+
+def login_view(request):
+
+    return render(request, "login_page.html")
+
+
+def signup_view(request):
+
+    return render(request, "signup_page.html")
